47f10b35700ab7b22508b2145bdbb0c173178805
manunit <hs1gab@gmail.com>
2021-08-26 13:20:50 +0700

    Version 1.2

--- ListView.php
+++ ListView.php
@@ -12,9 +12,10 @@ class Listview extends Controller
        public function index()
     {  
         $id = 35 ; 
-        $aaaa =  DB::select( 'CALL listview("2021-08-01","2021-08-31", ' . $id .'  )'  ) ;
-       
-        dd('test',$aaaa );
-        return view('user.index', ['users' => $users]);
+        $dateStart = '2021-07-01' ; 
+        $dateStop = '2021-08-31' ;
+        $aaaa =  DB::select( 'CALL listview("' . $dateStart. '","'. $dateStop .'",' . $id .')'  ) ;
+         dd('test',$aaaa );
+        return view('user.index', ['users' => $aaa]);
     }
 }
